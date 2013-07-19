f89d215a52e4e05745c2e629c86aa334ddff8ab0
Rigidus <avenger-f@yandex.ru>
2013-07-19 20:56:49 +0400

    trans users printing

--- trans.lisp
+++ trans.lisp
@@ -1,13 +1,14 @@
 (in-package #:motoinf)
 
-(define-condition pattern-not-found-error (error) (()))
+(define-condition pattern-not-found-error (error)
+  ((text :initarg :text :reader text)))
 
 (defun extract (cortege html)
   (loop :for (begin end regexp) :in cortege :collect
      (multiple-value-bind (start fin)
          (ppcre:scan regexp html)
        (when (null start)
-         (error 'pattern-not-found-error))
+         (error 'pattern-not-found-error :text regexp))
        (subseq html (+ start begin) (- fin end)))))
 
 (defun extract-voice-elt (id-str)
@@ -37,28 +38,45 @@
 
 
 (defun grab-user (id-str)
-  (let ((page (drakma:http-request (format nil "http://www.motobratan.ru/users/~A.html" id-str))))
-    (destructuring-bind (name flow age exp-raw)
-        (extract '((4 5 "<h1>.*</h1>")
-                   (23 6 "<div class=\"item flow\">.*</div>")
-                   (9 4 "Возраст: .* лет")
-                   (9 4 "(?s)Возраст: .*<div id=\\\"photos_id\\\">"))
-                 page)
-      (destructuring-bind (birthday exp)
-          (extract '((26 7 "<span class=\\\"small gray\\\">.*</span>")
-                     (11 4 "Мото-стаж: .* лет" ))
-                   exp-raw)
-        (list :name name
-              :flow flow
-              :age age
-              :birthday birthday
-              :exp exp
-              :motorcycles (mapcar #'(lambda (x)
-                                       (car (extract '((0 5 "\\d*.html")) x)))
-                                   (remove-duplicates
-                                    (ppcre:all-matches-as-strings
-                                     "<a href=\\\"/users/.*.*/motorcycles/.*html"
-                                     page)
-                                    :test #'equal)))))))
+  (let ((page (drakma:http-request (format nil "http://www.motobratan.ru/users/~A.html" id-str)))
+        age
+        exp-raw
+        birthday
+        exp)
+    (if (ppcre:all-matches-as-strings "анкета удалена" page)
+        nil
+        (destructuring-bind (name flow)
+            (extract '((4 5 "<h1>.*</h1>")
+                       (23 6 "<div class=\"item flow\">.*</div>"))
+                     page)
+          (handler-case
+              (let ((raw (extract '((9 4 "Возраст: .* (лет|год)")
+                                    (9 4 "(?s)Возраст: .*<div id=\\\"photos_id\\\">"))
+                                  page)))
+                (setf age (car raw))
+                (let ((raw2 (extract '((26 7 "<span class=\\\"small gray\\\">.*</span>")
+                                      (11 4 "Мото-стаж: .* лет" ))
+                                    (cadr raw))))
+                  (setf birthday (car raw2))
+                  (setf exp (cadr raw2))))
+            ;; error handler ignore
+            (pattern-not-found-error (err)
+              (format t "~%! pattern-not-found-warn: '~A'" (text err))))
+          (list :name name
+                :flow flow
+                :age age
+                :birthday birthday
+                :exp exp
+                :motorcycles (mapcar #'(lambda (x)
+                                         (car (extract '((0 5 "\\d*.html")) x)))
+                                     (remove-duplicates
+                                      (ppcre:all-matches-as-strings
+                                       "<a href=\\\"/users/.*/motorcycles/.*html"
+                                       page)
+                                      :test #'equal)))))))
 
-(grab-user "10001")
+(loop :for item :from 10001 :to 19110 :do
+   (print item)
+   (print (grab-user (format nil "~A" item))))
+
+(grab-user 10004)
